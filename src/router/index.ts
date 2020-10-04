ba9a304c98e5c788b8ae0ef127c73cfa4271f613
Eli Sokeland <etsokeland@bsu.edu>
2020-10-04 19:21:17 -0400

    Fixed router Typescript errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 const routerHistory = createWebHistory();
+
+//eslint-disable-next-line
+const routes = <Array<any>>[
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About
+  }
+];
 
 const router = createRouter({
   history: routerHistory,
-  routes: [
-    {
-      path: "/",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "../views/About.vue")
-    }
-  ]
+  routes: routes
 });
 
 export default router;
