7a7daa9198e4349a892fffd09149957198c14f0d
Eli Sokeland <etsokeland@bsu.edu>
2020-10-05 01:34:59 -0400

    Calculator Now Works

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/calculator">Calculator</router-link>
   </div>
   <router-view />
 </template>
@@ -15,6 +16,14 @@ export default defineComponent({
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
 #nav {
   padding: 30px;
 }
@@ -22,5 +31,9 @@ export default defineComponent({
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
